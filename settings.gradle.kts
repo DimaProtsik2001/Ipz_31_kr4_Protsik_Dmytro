e24b2cd414c03b0d2c715b5a89568fbf1fb077ea
Dimasik2_0_0_1 <dimaprocik2001@gmail.com>
2024-03-14 13:52:54 +0200

    Ініціалізація

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ipz_31_kr4_Protsik_Dmytro"
+include(":app")
+ 
